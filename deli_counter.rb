ed8b304cb07a376c5b0de25d26eb68f2eb919024
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 04:44:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,13 @@
 #if nobody in line, should say "The line is currently empty."
 
 def line(array)
-  if array.length == 0 
-    puts "The line is currently empty."
-  else array.length > 1 
-    message = "The line is currently:"
-    array.each_with_index do |index, name|
-    message += " #{index.to_i+1}. #{name}"
-  end
+    if array.length == 0 
+      puts "The line is currently empty."
+    else array.length > 1 
+      message = "The line is currently:"
+      array.each_with_index do |index, name|
+      message += " #{index.to_i+1}. #{name}"
+    end
   puts "#{message}"
+  end
 end
