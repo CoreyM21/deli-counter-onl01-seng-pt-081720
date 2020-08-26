ab74e85080b91f7fc0104ab738c9abdc4d2567c9
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 04:32:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   current_line = "The line is cuurently:"
   array.each_with_index(1) do |number, name| 
   current_line << " #{number}. #{name}"
-end
+  end
   "The line is currently empty."
  end
 end 
