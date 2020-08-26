b1e3bc7ff91d21752aa442dcd6ff91d18143e986
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 04:30:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,10 @@
 katz_deli =[]
 
 def line(array)
-  if array.length == 0 
+  current_line = "The line is cuurently:"
+  array.each_with_index(1) do |number, name| 
+  current_line << " #{number}. #{name}"
+  
   "The line is currently empty."
  end
 end 
